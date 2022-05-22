4c297dd6b7ccb8088ba27f0c1e60d08f98b5738e
Antoine D <Duwab@users.noreply.github.com>
2022-05-22 12:27:47 +0200

    Add nginx forground command

--- init-nginx.sh
+++ init-nginx.sh
@@ -6,3 +6,4 @@ cd repo
 if [ ! -z "$GIT_TAG" ]; then git checkout $GIT_TAG; fi
 cp ${CONFS_DIR:-./nginx/conf.d/}* /etc/nginx/conf.d || :
 cp -r ${WWW_DIR:-./nginx/html/}* /usr/share/nginx/html || :
+nginx -g 'daemon off;'
