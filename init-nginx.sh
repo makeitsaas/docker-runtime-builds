30fc8f6bfc8a86d217410919640f85c6f0091d19
Antoine Dubois <antoine.dubois@commandersact.com>
2022-05-22 13:27:12 +0200

    [init.sh] Only clone if directory missing
    When restarting a container, we don't need to re-clone.
    This also prevents error during restart

--- init-nginx.sh
+++ init-nginx.sh
@@ -1,7 +1,7 @@
 apt update && apt install -y git
 git config --global credential.helper "!f() { echo 'username=$GIT_USER'; echo 'password=$GIT_TOKEN'; };f"
 cd /srv
-git clone $REPO_URL repo
+if [ ! -d "./repo" ]; then git clone $REPO_URL repo; fi
 cd repo
 if [ ! -z "$GIT_TAG" ]; then git checkout $GIT_TAG; fi
 cp ${CONFS_DIR:-./nginx/conf.d}/* /etc/nginx/conf.d || :
--- init-node.sh
+++ init-node.sh
@@ -1,7 +1,7 @@
 apk add git
 git config --global credential.helper "!f() { echo 'username=$GIT_USER'; echo 'password=$GIT_TOKEN'; };f"
 cd /srv
-git clone $REPO_URL repo
+if [ ! -d "./repo" ]; then git clone $REPO_URL repo; fi
 cd repo
 if [ ! -z "$GIT_TAG" ]; then git checkout $GIT_TAG; fi
 if [ ! -z "$APP_DIR" ]; then cd $APP_DIR; fi
