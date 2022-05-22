cb6c26411bb0f28d45f3b24258e46738b121ca82
Antoine D <Duwab@users.noreply.github.com>
2022-05-22 12:43:37 +0200

    Move slashes

--- init-nginx.sh
+++ init-nginx.sh
@@ -4,6 +4,6 @@ cd /srv
 git clone $REPO_URL repo
 cd repo
 if [ ! -z "$GIT_TAG" ]; then git checkout $GIT_TAG; fi
-cp ${CONFS_DIR:-./nginx/conf.d/}* /etc/nginx/conf.d || :
-cp -r ${WWW_DIR:-./nginx/html/}* /usr/share/nginx/html || :
+cp ${CONFS_DIR:-./nginx/conf.d}/* /etc/nginx/conf.d || :
+cp -r ${WWW_DIR:-./nginx/html}/* /usr/share/nginx/html || :
 nginx -g 'daemon off;'
